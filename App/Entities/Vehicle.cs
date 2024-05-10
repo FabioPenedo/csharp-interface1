d0752e7df7895cf1cf4a4708ae04bbf9f3b178b7
FabioPenedo <fabiopenedo21@gmail.com>
2024-05-10 14:33:41 -0300

    solucao de problema sem interface

---
+++ App/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace App.Entities
+{
+    internal class Vehicle
+    {
+        public string Model { get; set; } = string.Empty;
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
